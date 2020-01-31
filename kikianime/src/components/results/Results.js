ab5771a2f3069fb5fb9cca3a475acf6cb8ceab96
unknown <avnvbhatta@gmail.com>
2020-01-30 21:11:40 -0800

    basic layout

---
+++ kikianime/src/components/results/Results.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Results.scss";
+
+function Results(){
+    return(
+        <div>
+            <p>This is the results component.</p>
+        </div>
+    )
+}
+
+export default Results;

f46b7141b06247bfb11be75e43d2b45ee39f69c0
unknown <avnvbhatta@gmail.com>
2020-01-30 23:31:53 -0800

    working random image picker

--- kikianime/src/components/results/Results.js
+++ kikianime/src/components/results/Results.js
@@ -1,12 +1,57 @@
 import React from "react";
 import "./Results.scss";
 
-function Results(){
-    return(
-        <div>
-            <p>This is the results component.</p>
-        </div>
-    )
+const imgArray = [ 
+    "/images/unity.jpg",
+    "/images/moodmate.png",
+    "/images/sajjan.png",
+    "/images/download.png"
+]
+
+
+class Results extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            images: [],
+            selectedImage: "",
+            loaded: false
+        }
+    }
+
+    componentDidMount(){
+        setInterval(() => {
+            this.setState({
+                images: imgArray,
+                selectedImage : imgArray[Math.floor(Math.random()*imgArray.length)],
+                loaded: true
+            })
+        }, 2000);
+        
+    }
+
+    getDiffImages(){
+        let imagesDiv = [];
+        for(let i=0; i<this.state.images.length; i++){
+            imagesDiv.push(
+                <img src={this.state.images[Math.floor(Math.random()*imgArray.length)]} alt=""/>
+            )
+        }
+        return imagesDiv;
+    }
+    
+    render(){
+        return(
+            <div>
+                <div className="fill"></div>
+                <p>This is the results component.</p>
+                {
+                    !this.state.loaded ? <p>Loading</p> : this.getDiffImages()
+                }
+            </div>
+        )
+    }
+    
 }
 
 export default Results;
